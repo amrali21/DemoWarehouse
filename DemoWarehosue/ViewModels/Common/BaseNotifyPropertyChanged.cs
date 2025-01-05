583377440b65f8d89ee7963a30822c51b35fcf58
Amr Moneer <Amr-Monir@AMR-MONIR>
2025-01-05 15:18:45 +0200

    make main view model

---
+++ DemoWarehosue/ViewModels/Common/BaseNotifyPropertyChanged.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace SpectroBridge.ViewModel
+{
+    public abstract class BaseNotifyPropertyChanged : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ DemoWarehosue/ViewModels/MainViewModel.cs
@@ -0,0 +1,28 @@
+﻿using DemoWarehosue.Models;
+using DemoWarehosue.Models.Repository;
+using SpectroBridge.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoWarehosue.ViewModels
+{
+    class MainViewModel: BaseNotifyPropertyChanged
+    {
+        // model for list body
+        // 
+        public List<Item> allItems { get; set; }
+
+        public MainViewModel()
+        {
+            //allItems = await RepoWrapper.Instance.itemsRepository.GetAllAsync()
+        }
+
+        void fetchAllItems()
+        {
+
+        }
+    }
+}
