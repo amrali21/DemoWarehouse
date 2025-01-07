bcb1b95c17075a5083b0dd7cadb6250c134d4abe
Amr Moneer <Amr-Monir@AMR-MONIR>
2025-01-07 15:09:58 +0200

    test combobox

--- DemoWarehosue/Views/InputForm.xaml.cs
+++ DemoWarehosue/Views/InputForm.xaml.cs
@@ -2,9 +2,12 @@
 using DemoWarehosue.Models.Repository;
 using DemoWarehosue.Models.UI;
 using DemoWarehosue.ViewModels;
+using SpectroBridge.ViewModel;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -27,8 +30,39 @@ namespace DemoWarehosue.Views
         public InputForm()
         {
             InitializeComponent();
-            
+            _ = loadData();
         }
+
+        async Task loadData()
+        {
+            try
+            {
+                await Task.Delay(1000);
+                Categories = await RepoWrapper.Instance.gategoryRepository.GetAllAsync();
+            }
+            catch (Exception ex) 
+            { 
+            
+            }
+        }
+
+
+        string _TestProp = "test label content";
+        public string TestProp { get => _TestProp; set { _TestProp = value; OnPropertyChanged(nameof(TestProp)); } }
+
+
+        List<string> _tests = new List<string> { "hi", "hi2", "hi3" };
+        public List<string> tests { get => _tests; set { _tests = value; OnPropertyChanged(nameof(tests)); } }
+
+
+        List<Category> _Categories;
+        public List<Category> Categories { get => _Categories; set { _Categories = value; OnPropertyChanged(nameof(Categories)); } }
+
+
+        Category _SelectedCategory;
+        Category SelectedCategory { get => _SelectedCategory; set { _SelectedCategory = value; OnPropertyChanged(nameof(SelectedCategory)); } }
+
+
 
         public static readonly DependencyProperty MyItemProperty =
             DependencyProperty.Register("MyItem", typeof(PutItem), typeof(InputForm), new PropertyMetadata(new PutItem()));
@@ -88,6 +122,13 @@ namespace DemoWarehosue.Views
         {
             EditMode = false;
             Reset();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
